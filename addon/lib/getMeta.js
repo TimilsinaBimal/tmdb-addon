063310b195e5a263be82bf240ca40aff285a503c
Bimal Timilsina <timilsina.bml05@gmail.com>
2025-03-02 15:46:46 +0545

    fix: wrong imdb_rating on links

--- addon/lib/getMeta.js
+++ addon/lib/getMeta.js
@@ -15,8 +15,9 @@ const blacklistLogoUrls = [
 async function getMeta(type, language, tmdbId, rpdbkey) {
   if (type === "movie") {
     const meta = await moviedb
-      .movieInfo({id: tmdbId, language, append_to_response: "videos,credits",})
+      .movieInfo({ id: tmdbId, language, append_to_response: "videos,credits", })
       .then(async (res) => {
+        const imdbRating = res.imdb_id ? await getImdbRating(res.imdb_id, type) : res.vote_average.toFixed(1);
         const resp = {
           imdb_id: res.imdb_id,
           cast: Utils.parseCast(res.credits),
@@ -24,7 +25,7 @@ async function getMeta(type, language, tmdbId, rpdbkey) {
           description: res.overview,
           director: Utils.parseDirector(res.credits),
           genre: Utils.parseGenres(res.genres),
-          imdbRating: res.imdb_id ? await getImdbRating(res.imdb_id, type) : res.vote_average.toFixed(1),
+          imdbRating: imdbRating,
           name: res.title,
           released: new Date(res.release_date),
           slug: Utils.parseSlug(type, res.title, res.imdb_id),
@@ -40,7 +41,7 @@ async function getMeta(type, language, tmdbId, rpdbkey) {
           releaseInfo: res.release_date ? res.release_date.substr(0, 4) : "",
           trailerStreams: Utils.parseTrailerStream(res.videos),
           links: new Array(
-            Utils.parseImdbLink(res.vote_average, res.imdb_id),
+            Utils.parseImdbLink(imdbRating, res.imdb_id),
             Utils.parseShareLink(res.title, res.imdb_id, type),
             ...Utils.parseGenreLink(res.genres, type, language),
             ...Utils.parseCreditsLink(res.credits)
@@ -52,7 +53,7 @@ async function getMeta(type, language, tmdbId, rpdbkey) {
         };
         try {
           resp.logo = await getLogo(tmdbId, language, res.original_language);
-        } catch(e) {
+        } catch (e) {
           console.log(`warning: logo could not be retrieved for ${tmdbId} - ${type}`);
           console.log((e || {}).message || "unknown error");
         }
@@ -68,14 +69,15 @@ async function getMeta(type, language, tmdbId, rpdbkey) {
     return Promise.resolve({ meta });
   } else {
     const meta = await moviedb
-      .tvInfo({id: tmdbId, language, append_to_response: "videos,credits,external_ids",})
+      .tvInfo({ id: tmdbId, language, append_to_response: "videos,credits,external_ids", })
       .then(async (res) => {
+        const imdbRating = res.external_ids.imdb_id ? await getImdbRating(res.external_ids.imdb_id, type) : res.vote_average.toFixed(1)
         const resp = {
           cast: Utils.parseCast(res.credits),
           country: Utils.parseCoutry(res.production_countries),
           description: res.overview,
           genre: Utils.parseGenres(res.genres),
-          imdbRating: res.external_ids.imdb_id ? await getImdbRating(res.external_ids.imdb_id, type) : res.vote_average.toFixed(1),
+          imdbRating: imdbRating,
           imdb_id: res.external_ids.imdb_id,
           name: res.name,
           poster: await Utils.parsePoster(type, tmdbId, res.poster_path, language, rpdbkey),
@@ -92,7 +94,7 @@ async function getMeta(type, language, tmdbId, rpdbkey) {
           releaseInfo: Utils.parseYear(res.status, res.first_air_date, res.last_air_date),
           videos: [],
           links: new Array(
-            Utils.parseImdbLink(res.vote_average, res.external_ids.imdb_id),
+            Utils.parseImdbLink(imdbRating, res.external_ids.imdb_id),
             Utils.parseShareLink(res.name, res.external_ids.imdb_id, type),
             ...Utils.parseGenreLink(res.genres, type, language),
             ...Utils.parseCreditsLink(res.credits)
@@ -106,7 +108,7 @@ async function getMeta(type, language, tmdbId, rpdbkey) {
         };
         try {
           resp.logo = await getTvLogo(res.external_ids.tvdb_id, res.id, language, res.original_language);
-        } catch(e) {
+        } catch (e) {
           console.log(`warning: logo could not be retrieved for ${tmdbId} - ${type}`);
           console.log((e || {}).message || "unknown error");
         }
@@ -118,7 +120,7 @@ async function getMeta(type, language, tmdbId, rpdbkey) {
         }
         try {
           resp.videos = await getEpisodes(language, tmdbId, res.external_ids.imdb_id, res.seasons);
-        } catch(e) {
+        } catch (e) {
           console.log(`warning: episodes could not be retrieved for ${tmdbId} - ${type}`);
           console.log((e || {}).message || "unknown error");
         }
