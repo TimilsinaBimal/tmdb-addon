d117d227528b7ad07a4765de679cc18c2ef15a2a
Bimal Timilsina <timilsina.bml05@gmail.com>
2025-03-06 14:01:07 +0545

    one decimal point vote average

--- addon/lib/getMeta.js
+++ addon/lib/getMeta.js
@@ -18,8 +18,8 @@ async function getMeta(type, language, tmdbId, rpdbkey) {
       .movieInfo({id: tmdbId, language, append_to_response: "videos,credits",})
       .then(async (res) => {
         const imdbRating = res.imdb_id
-          ? await getImdbRating(res.imdb_id, type) ?? res.vote_average.toString()
-          : res.vote_average.toString();
+          ? await getImdbRating(res.imdb_id, type) ?? res.vote_average.toFixed(1).toString()
+          : res.vote_average.toFixed(1).toString();
         const resp = {
           imdb_id: res.imdb_id,
           cast: Utils.parseCast(res.credits),
@@ -74,8 +74,8 @@ async function getMeta(type, language, tmdbId, rpdbkey) {
       .tvInfo({id: tmdbId, language, append_to_response: "videos,credits,external_ids",})
       .then(async (res) => {
         const imdbRating = res.external_ids.imdb_id
-          ? await getImdbRating(res.external_ids.imdb_id, type) ?? res.vote_average.toString()
-          : res.vote_average.toString();
+          ? await getImdbRating(res.external_ids.imdb_id, type) ?? res.vote_average.toFixed(1).toString()
+          : res.vote_average.toFixed(1).toString();
         const runtime = res.episode_run_time?.[0] ?? res.last_episode_to_air?.runtime ?? res.next_episode_to_air?.runtime ?? null;
         const resp = {
           cast: Utils.parseCast(res.credits),
