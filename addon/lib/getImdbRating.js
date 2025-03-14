e446020c11e59e5b1ede63ff1b5081972edb24d4
Bimal Timilsina <timilsina.bml05@gmail.com>
2025-03-14 18:32:55 +0545

    imdb rating

--- addon/lib/getImdbRating.js
+++ addon/lib/getImdbRating.js
@@ -4,7 +4,7 @@ async function getImdbRating(imdbId, type) {
   try {
     const response = await axios.get(`https://cinemeta-live.strem.io/meta/${type}/${imdbId}.json`);
     const data = response.data.meta;
-    return data?.imdbRating || "N/A";
+    return data.imdbRating;
   } catch (error) {
     console.error('Erro ao obter dados do Cinemeta:', error);
     throw error;
